e3d9bbc9327c0f72bee5aa34d0336da88946e11e
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-03 20:19:19 +0200

    enunciado funcion 2

--- funciones.py
+++ funciones.py
@@ -3,11 +3,14 @@ Durante el curso se realizaron dos exámenes parciales de teoría y un examen de
 Los alumnos que tuvieron menos de 4 en alguno de estos exámenes pudieron repetirlo en la al final del
 curso (convocatoria ordinaria).
 Escribir un programa que contenga las siguientes funciones:
-
-Una función que reciba el fichero de calificaciones 
-y devuelva una lista de diccionarios, donde cada diccionario contiene la información de los exámenes y 
-la asistencia de un alumno. La lista tiene que estar ordenada por apellidos.
 '''
+'''
+Una función que reciba una lista de diccionarios como la que devuelve la función anterior y 
+añada a cada diccionario un nuevo par con la nota final del curso. 
+El peso de cada parcial de teoría en la nota final es de un 
+30% mientras que el peso del examen de prácticas es de un 40%.
+'''
+
 import csv
 #import sys  sys.getdefaultencoding() nos dice en que formato está
 import operator
